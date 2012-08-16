3e58d94a3dc768abb4098fec0edba10000b4e622
Niko Neufeld <niko.neufeld@gmail.com>
2012-08-16 23:34:28 +0200

    Initial Commit

---
+++ slm/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  slm
+//
+//  Created by Niko Neufeld on 8/16/12.
+//  Copyright (c) 2012 Niko Neufeld. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
